12001d52d6a99b6ae4534ae2c185f59627621701
GastonPetiti <sgastonpetiti@gmail.com>
2023-11-08 18:40:15 -0300

    agregando footer y corrigiendo navbar

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,17 @@
-import { Flex } from "@chakra-ui/react"
+import { Flex, Text } from "@chakra-ui/react"
+
 
 export const Footer = () => {
   return (
-    <Flex height={'10vh'} width={'100%'} bg={"teal.900"} color={'orange.50'} justify={'center'} align={'center'}>
-      footer
+    <Flex justify={'center'} align={'center'} direction={'column'}>
+
+    <Flex height={'10vh'} width={'100%'} bg={"teal.900"} color={'orange.50'} justify={'center'} align={'center'} wrap={'wrap'}>
+        <li><a href="#">Inicio</a></li>
+        <li><a href="#">Productos</a></li>
+        <li><a href="#">Contacto</a></li>
+        <li><a href="#">Nosotros</a></li>
+      </Flex>
+      <Text bg={"teal.900"} w={'100%'} textAlign={'center'} color={'orange.50'}>Sitio hecho por <a href="http://https://www.linkedin.com/in/gastonpetiti/" target="_blank" rel="noopener noreferrer">Gastón Petiti</a></Text>
     </Flex>
   )
 }
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,15 +2,15 @@ import { Flex, Box } from '@chakra-ui/react'
 
 export const Header = () => {
   return (
-    <Flex height={'10vh'} width={'100%'} bg={'teal.900'} align={'center'} justify={'space-between'} direction={'row'} color='orange.50' m={0}>
-      <Box m={3}>
+    <Flex height={'10vh'} width={'100%'} bg={'teal.900'} align={'center'} justify={'space-between'} direction={'row'} color='orange.50' p={2}>
+      <Box>
         CelularesStore
       </Box>
-      <Flex mr={6}>
-        <li>Inicio</li>
-        <li>Productos</li>
-        <li>Contacto</li>
-        <li>Nosotros</li>
+      <Flex maxW={'20rem'}>
+        <li><a href="#">Inicio</a></li>
+        <li><a href="#">Productos</a></li>
+        <li><a href="#">Contacto</a></li>
+        <li><a href="#">Nosotros</a></li>
       </Flex>
     </Flex>
     )
