d56335d16578de207cea098ac6f428f677a5599f
GastonPetiti <sgastonpetiti@gmail.com>
2023-11-14 00:22:12 -0300

    modificando links

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,18 +1,43 @@
-import { Flex, Text } from "@chakra-ui/react"
-
+import { Flex, Text } from "@chakra-ui/react";
 
 export const Footer = () => {
   return (
-    <Flex justify={'center'} align={'center'} direction={'column'}>
-
-    <Flex height={'10vh'} width={'100%'} bg={"teal.900"} color={'orange.50'} justify={'center'} align={'center'} wrap={'wrap'}>
-        <li><a href="#">Inicio</a></li>
-        <li><a href="#">Productos</a></li>
-        <li><a href="#">Contacto</a></li>
-        <li><a href="#">Nosotros</a></li>
+    <Flex justify={"center"} align={"center"} direction={"column"}>
+      <Flex
+        height={"10vh"}
+        width={"100%"}
+        bg={"teal.900"}
+        color={"orange.50"}
+        justify={"center"}
+        align={"center"}
+        wrap={"wrap"}
+      >
+        <ul className="linksContainer">
+          <li>
+            <a href="#">Inicio</a>
+          </li>
+          <li>
+            <a href="#">Productos</a>
+          </li>
+          <li>
+            <a href="#">Contacto</a>
+          </li>
+          <li>
+            <a href="#">Nosotros</a>
+          </li>
+        </ul>
       </Flex>
-      <Text bg={"teal.900"} w={'100%'} textAlign={'center'} color={'orange.50'}>Sitio hecho por <a href="http://https://www.linkedin.com/in/gastonpetiti/" target="_blank" rel="noopener noreferrer">Gastón Petiti</a></Text>
+      <Text bg={"teal.900"} w={"100%"} textAlign={"center"} color={"orange.50"}>
+        Sitio hecho por{" "}
+        <a
+          href="http://https://www.linkedin.com/in/gastonpetiti/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Gastón Petiti
+        </a>
+      </Text>
     </Flex>
-  )
-}
-export default Footer
+  );
+};
+export default Footer;
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,18 +2,35 @@ import { Flex, Box } from '@chakra-ui/react'
 
 export const Header = () => {
   return (
-    <Flex height={'10vh'} width={'100%'} bg={'teal.900'} align={'center'} justify={'space-between'} direction={'row'} color='orange.50' p={2}>
-      <Box>
-        CelularesStore
-      </Box>
-      <Flex maxW={'20rem'}>
-        <li><a href="#">Inicio</a></li>
-        <li><a href="#">Productos</a></li>
-        <li><a href="#">Contacto</a></li>
-        <li><a href="#">Nosotros</a></li>
+    <Flex
+      height={"10vh"}
+      width={"100%"}
+      bg={"teal.900"}
+      align={"center"}
+      justify={"space-between"}
+      direction={"row"}
+      color="orange.50"
+      p={2}
+    >
+      <Box>CelularesStore</Box>
+      <Flex maxW={"fit-content"}>
+        <ul className="linksContainer">
+          <li>
+            <a href="#">Inicio</a>
+          </li>
+          <li>
+            <a href="#">Productos</a>
+          </li>
+          <li>
+            <a href="#">Contacto</a>
+          </li>
+          <li>
+            <a href="#">Nosotros</a>
+          </li>
+        </ul>
       </Flex>
     </Flex>
-    )
+  );
 }
 
 
